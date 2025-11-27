efcc225a1d746a537194b6cee600de51169d7bb8
diudiuyc5200 <17766728994@163.com>
2025-11-27 20:40:44 +0800

    Update page_alloc.c

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -317,7 +317,7 @@ compound_page_dtor * const compound_page_dtors[] = {
  * allocations below this point, only high priority ones. Automatically
  * tuned according to the amount of memory in the system.
  */
-int min_free_kbytes = 10900;
+int min_free_kbytes = 8912;
 int user_min_free_kbytes = -1;
 int watermark_scale_factor = 500;
 
